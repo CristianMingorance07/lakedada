3e7e8658f492fee3545a7c3b82dff7596f460d03
CORP\cmingora <cristian.mingorance-mulero@capgemini.com>
2024-11-13 15:47:54 +0100

    commit 6

--- la-kedada-landing/src/app/app.component.ts
+++ la-kedada-landing/src/app/app.component.ts
@@ -7,6 +7,7 @@ import {AboutComponent} from "./components/about/about.component";
 import {BannerComponent} from "./components/banner/banner.component";
 import {HeaderComponent} from "./components/header/header.component";
 import {NgClass} from "@angular/common";
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-root',
@@ -22,5 +23,23 @@ export class AppComponent {
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.pageYOffset > 50;
+  }
+
+
+  sendEmail(event: Event) {
+    event.preventDefault();
+
+    emailjs.sendForm(
+      'service_zwvbkgf',
+      'template_aumru0e',
+      event.target as HTMLFormElement,
+      'xC2bKaNewUtskFKsA'
+    ).then((result: EmailJSResponseStatus) => {
+      console.log('Correo enviado:', result.text);
+      alert('Mensaje enviado correctamente.');
+    }, (error) => {
+      console.log('Error al enviar correo:', error.text);
+      alert('Hubo un error al enviar el mensaje. Inténtalo nuevamente.');
+    });
   }
 }
